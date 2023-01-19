ae348957135413cf05710f95cf504442932acace
Andrea-Rivera <36813115+Andrea-Rivera@users.noreply.github.com>
2023-01-19 15:41:05 +1000

    Update home

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,30 +1,26 @@
 import HomeImage from "../projectList/images/picAndrea.JPG";
 import CV from "../projectList/images/cvNew2022.pdf";
 import { HiDownload } from "react-icons/hi";
-import homeStyles from "./home.module.css";
+import "./home.css";
 
 const Home = () => {
   return (
-    <section style={{ height: "100vh" }}>
+    <section>
+      <h1> Andrea Rivera Pena </h1>
+      <div className="header">
+        <img src={HomeImage} alt="Portfolio Projects" className="homeImage" />
+      </div>
       <article className="home">
-        <h1> Andrea Rivera Pena </h1>
         <h2>Changing careers to become a great Programmer & Web Developer</h2>
-        <img
-          className={homeStyles.img}
-          src={HomeImage}
-          alt="Andrea Rivera"
-          style={{ alignItems: "center", height: "20vh", width: "20vh" }}
-        />
 
-        <p className={homeStyles.p}>
+        <p className="homeParagraph">
           "I am a career changer and a student of Code like a Girl scholarship.
           Currently, I am looking for opportunities to build more expertise in
           the web development industry to become an experienced programmer. I
           would like to become an expert in Front-end development. Then, a
           professional full stack developer".
         </p>
-
-        <a href={CV} download className="btn primary">
+        <a href={CV} download className="btn">
           Download CV
           <HiDownload />
         </a>
