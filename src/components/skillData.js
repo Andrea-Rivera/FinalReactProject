b1bcef820a1e00c432b0e228e2e5b409c4136ab3
Andrea-Rivera <36813115+Andrea-Rivera@users.noreply.github.com>
2023-04-03 14:36:38 +1000

    update skilldata.js

--- src/components/skillData.js
+++ src/components/skillData.js
@@ -3,6 +3,9 @@ import { SiReact } from "react-icons/si";
 import { ImHtmlFive } from "react-icons/im";
 import { SiCsswizardry } from "react-icons/si";
 import { SiSharp } from "react-icons/si";
+import htmlImage from "./card/skillcard/images/html.png";
+import jsImage from "./card/skillcard/images/js.png";
+import reactImage from "./card/skillcard/images/react.png";
 
 const Skilldata = [
   {
@@ -10,10 +13,18 @@ const Skilldata = [
     icon: <ImHtmlFive />,
     title: "HTML",
     description:
-      "I have learned to structure images, links ,div, span, tables, inputs and some modern concepts such as headers,articles,sections. I believe that I have a good understanding of HTML",
-       certificate: (
-      <a href="https://api.au.badgr.io/public/assertions/txVo6Yy2RiGtMTBnpsG30w?identity__email=andita54%40hotmail.com">
-        HTML & CSS Badge
+      "I learned to structure images, links ,div, span, tables, inputs and other concepts such as headers,articles,sections.",
+    certificate: (
+      <a
+        href="https://api.au.badgr.io/public/assertions/txVo6Yy2RiGtMTBnpsG30w?identity__email=andita54%40hotmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={htmlImage}
+          alt="HTML & CSS badge"
+          style={{ width: "80px", height: "80px" }}
+        />
       </a>
     ),
   },
@@ -22,17 +33,25 @@ const Skilldata = [
     icon: <SiCsswizardry />,
     title: "CSS",
     description:
-      "I have learned about colours, fonts, css selectors, responsive design , flexbox, accesibility",
+      "I learned about colours, fonts, css selectors, responsive design , flexbox, accesibility",
   },
   {
     id: 3,
     icon: <SiJavascript />,
     title: "Javascript",
     description:
-      "I have learned variables, data types,loops, functions , Web Apis, arrays, objects, ajax and fetch Api",
-      certificate: (
-      <a href="https://api.au.badgr.io/public/assertions/-xxscIJjSqap3Psy7BtmlA?identity__email=andita54%40hotmail.com">
-        Javascript Badge
+      "I learned variables, data types,loops, functions , Web Apis, arrays, objects, ajax and fetch Api",
+    certificate: (
+      <a
+        href="https://api.au.badgr.io/public/assertions/-xxscIJjSqap3Psy7BtmlA?identity__email=andita54%40hotmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={jsImage}
+          alt="HTML & CSS badge"
+          style={{ width: "60px", height: "60px" }}
+        />
       </a>
     ),
   },
@@ -41,10 +60,18 @@ const Skilldata = [
     icon: <SiReact />,
     title: "React",
     description:
-      "I have learned about ES6, components, props, routing, working with APIs, react hooks such as useState, useEffect, useContext ",
-      certificate: (
-      <a href="https://api.au.badgr.io/public/assertions/sWI0AKS8TBuGZsS4LJL4Ag?identity__email=andita54%40hotmail.com">
-        React Badge
+      "I learned about ES6, components, props, routing, working with APIs, react hooks such as useState, useEffect, useContext ",
+    certificate: (
+      <a
+        href="https://api.au.badgr.io/public/assertions/sWI0AKS8TBuGZsS4LJL4Ag?identity__email=andita54%40hotmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={reactImage}
+          alt="HTML & CSS badge"
+          style={{ width: "60px", height: "60px" }}
+        />
       </a>
     ),
   },
@@ -52,7 +79,8 @@ const Skilldata = [
     id: 5,
     icon: <SiSharp />,
     title: "C#",
-    description:"I have learned about classes, methods, properties and concepts like inheritance and abstraction",
+    description:
+      "I learned about classes, methods, properties and concepts like inheritance and abstraction.Now am learning and doing .Net Core applications",
   },
 ];
 
