1226bcdc38159d472afc64b78ee1d1d5b875bc4a
Andrea-Rivera <36813115+Andrea-Rivera@users.noreply.github.com>
2023-04-27 12:42:27 +1000

    delete index loading spinner

--- src/components/loadingSpinner/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import classes from "./LoadingSpinner.module.css";
-
-export default function LoadingSpinner() {
-  return <div className={classes["loading-spinner"]}></div>;
-}
