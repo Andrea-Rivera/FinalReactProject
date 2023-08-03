51427c95e4681749c7ac40f38638253f64ef83fd
Andrea-Rivera <36813115+Andrea-Rivera@users.noreply.github.com>
2023-08-03 13:44:20 +1000

    Update about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,7 +2,7 @@ import Skills from "../skills";
 
 const About = () => {
   return (
-    <section style={{ height: "200vh" }}>
+    <section style={{ height: "250vh" }}>
       <Skills />
     </section>
   );
